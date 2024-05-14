2fc7d0ccba6c1df0fa2301737951e1a5ffffacd0
BaekGeunYoung <dvmflstm@gmail.com>
2024-05-15 01:11:13 +0900

    snaphot publish test

--- src/main/java/io/portone/webhook/WebhookSigningException.java
+++ src/main/java/io/portone/webhook/WebhookSigningException.java
@@ -1,6 +1,8 @@
 package io.portone.webhook;
 
 public class WebhookSigningException extends Exception {
+	@java.io.Serial
+    private static final long serialVersionUID = -3387516993124229948L;
 	public WebhookSigningException(final String message) {
 		super(message);
 	}
--- src/main/java/io/portone/webhook/WebhookVerificationException.java
+++ src/main/java/io/portone/webhook/WebhookVerificationException.java
@@ -1,6 +1,8 @@
 package io.portone.webhook;
 
 public class WebhookVerificationException extends Exception {
+	@java.io.Serial
+	private static final long serialVersionUID = -3387516993124229948L;
 	public WebhookVerificationException(final String message) {
 		super(message);
 	}
